cddebcb7c0ac96e7d68bcc68d34df9f654c5d48a
Kapil Chaudhary <108571992+Kapil685838@users.noreply.github.com>
2023-04-28 15:32:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 //your JS code here. If required.
+
+function randomPromise() {
+	return new Promise((reject, resolve) => {
+		let time = Math.floor(Math.random() * 3) + 1;
+		setTimeout(() => {
+			resolve(time);
+		}, time * 1000);
+	});
+}
+
+const promises = [randomPromise(), randomPromise(), randomPromise()];
+
+Promise.all(promises)
+	.then((res) => {
+		const [time1, time2, time3] = res;
+		const total = time1 + time2 + time3;
+		const tableBody = document.getElementById('#output');
+		tableBody.innerHTML = `
+			<tr>
+			   <td>Promise 1</td>
+			   <td>${time1}</td>
+			</tr>
+			<tr>
+			   <td>Promise 1</td>
+			   <td>${time2}</td>
+			</tr>
+            <tr>
+			   <td>Promise 1</td>
+			   <td>${time3}</td>
+			</tr>
+			<tr>
+			   <td>Promise 1</td>
+			   <td>${total}</td>
+			</tr>
+		`;
+	});
